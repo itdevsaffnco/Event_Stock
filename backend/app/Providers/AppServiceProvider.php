a4bbb0245a4325967639251c80bcf853175665ee
dwikisaffnco <dwiki@saffnco.com>
2026-05-21 16:58:42 +0700

    Add Docker support and CI/CD workflow for frontend and backend
    - Create Dockerfile for backend with necessary PHP extensions and dependencies
    - Create Dockerfile for frontend with build steps and production configuration
    - Add GitHub Actions workflow for building and deploying frontend and backend images
    - Update .gitignore files for both frontend and backend to include relevant files

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,23 +2,22 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if (config('app.env') === 'production') {
+            $url->forceScheme('https');
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
